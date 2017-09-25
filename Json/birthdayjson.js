c412f59e342da5891cd0de67092769f26ad6b9f0
qiusunshine <1596889407@qq.com>
2017-09-25 12:42:55 +0800

    Create birthdayjson.js

---
+++ Json/birthdayjson.js
@@ -0,0 +1,22 @@
+[
+    {
+        "name": "孙林",
+        "detail": "熊秋秋，祝你生日快乐！同时也祝你和德银长长久久。",
+        "music": "http://music.163.com/song/439915614/?userid=361277183",
+        "type": "3",
+        "head": "http://suo.im/2Ak0WX",
+        "img1": "http://suo.im/vfCpS",
+        "img2": "http://suo.im/2iYo2q",
+        "img3": "http://suo.im/2CnutI"
+    },
+    {
+        "name": "赵梓强",
+        "detail": "飞花飘絮，霓裳翩翩起舞，几多情愫心飞扬。广袖流云，琴曲指夹凝，清水芙蕖脱尘嚣。",
+        "music": "nomusic",
+        "type": "3",
+        "head": "http://suo.im/2Z5D4a",
+        "img1": "http://suo.im/4xgan4",
+        "img2": "http://suo.im/3s53JZ",
+        "img3": "http://suo.im/3cAISf"
+    }
+]
