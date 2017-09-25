ebadade6ee880fb360cea43955f22ea07b16e1e7
qiusunshine <1596889407@qq.com>
2017-09-25 22:42:42 +0800

    Update birthdayjson.js

--- Json/birthdayjson.js
+++ Json/birthdayjson.js
@@ -31,21 +31,51 @@
     },
     {
         "name": "蓝勇",
-        "detail": "早点和我兄弟结婚，这杯酒都等了好几年了，熊大，生日快乐!",
+        "detail": "早点和我兄弟结婚，这杯酒都等了好几年了，熊大，生日快乐",
         "music": "https://c.y.qq.com/v8/playsong.html?songmid=004XzVtI1YsLfx&ADTAG=myqq&from=myqq&channel=10007100",
         "type": "1",
         "head": "http://suo.im/3yExFq",
         "img1": "http://suo.im/17mDvm",
         "img2": "",
         "img3": ""
-    }, 
-      {
+    },
+    {
         "name": "何宇",
-        "detail": "点右上角看何二妹给猫猫的信",
+        "detail": "点右上角查看何二妹给猫猫的信",
         "music": "http://viewer.shouzhangapp.com/H5share/1090579_9YA9USKRZY?app=shouzhang",
         "type": "1",
-        "head": "http://suo.im/3iOkjL",
+        "head": "http://suo.im/37ncuL",
         "img1": "http://suo.im/iLeyo",
+        "img2": "",
+        "img3": ""
+    },
+    {
+        "name": "杨利",
+        "detail": "愿你：在此后的人生，幸运会如影随形，能够拥有一生一世的追求；让幸福伴随你慢慢成功。你的未来会有很大美丽的惊喜，就像你会渐渐长成一位美丽可爱的他要倾尽此生守护的菇凉，想把所有的祝福都给你，例如：“生日快乐，秋秋！”",
+        "music": "nomusic",
+        "type": "3",
+        "head": "http://suo.im/4ldkKB",
+        "img1": "http://suo.im/4Dm9yv",
+        "img2": "http://suo.im/3fuSh",
+        "img3": "http://suo.im/4ldkKB"
+    },
+    {
+        "name": "夏永峰",
+        "detail": "何大嫂，生日快乐！在接下来的岁月里，希望你的事业蒸蒸日上，微商店越做越大，身价超千万。还有和何大哥，岁数到了就赶紧把证儿办了，咱们都等着喝喜酒了，我们非常看好你俩噢！微薄祝福，言辞力弱，但礼轻情意重，望安好！",
+        "music": "https://c.y.qq.com/v8/playsong.html?songmid=002BIjxb1Erg6e&ADTAG=myqq&from=myqq&channel=10007100",
+        "type": "3",
+        "head": "http://suo.im/2E1G3c",
+        "img1": "http://suo.im/1VYanX",
+        "img2": "http://suo.im/dQ6rC",
+        "img3": "http://suo.im/2Y03zw"
+    },
+    {
+        "name": "陈洋-林嫂",
+        "detail": "如果风可以解语，我愿意\n为你向风借来，快乐的源泉\n就在今天,为你写下祝福\n秋,生日快乐",
+        "music": "nomusic",
+        "type": "2",
+        "head": "http://suo.im/2bsb38",
+        "img1": "",
         "img2": "",
         "img3": ""
     }
