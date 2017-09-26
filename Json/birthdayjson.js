6460352292e7d0f9f00c40ef444d46d914bca55a
qiusunshine <1596889407@qq.com>
2017-09-27 00:29:15 +0800

    Update birthdayjson.js

--- Json/birthdayjson.js
+++ Json/birthdayjson.js
@@ -2,44 +2,44 @@
       {
         "name": "小棉袄",
         "detail": "秋秋，祝你生日快乐！一起努力，早点过四级，拿到所有想要的证，还要有那个证哦；去吃遍天下美食，游最美风景；家庭琐事，我们不管，烦恼忧愁，全都抛弃；还有，我们永不分离。\n--2017/09/24",
-        "music": "https://c.y.qq.com/v8/playsong.html?songmid=000LBcVm0d9raf&ADTAG=myqq&from=myqq&channel=10007100",
+        "music": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/595fe43c6da3af76.jpg",
         "type": "3",
-        "head": "https://media.everphoto.cn/180x180/6470102692348822029.webp?media_token=374a1656d98614cfcfb4e047b819630f",
+        "head": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/595fe43c6da3af76.jpg",
         "qq": "1596889407",
-        "img1": "https://media.everphoto.cn/180x180/6469593849156076046.webp?media_token=9c004bcf61bbc7ec69cf69980fe13dc6",
-        "img2": "https://media.everphoto.cn/180x180/6469593810774000141.webp?media_token=b26cd7eb2af4dc85fe31ace6c8aa4a68",
-        "img3": "https://media.everphoto.cn/180x180/6469593786627392014.webp?media_token=4ea3d3ee52d3d769b411753790e1ba5e"
+        "img1": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/EVERPHOTO_7c2aabcbe1f5089af7bb7ef1a544ff1a.jpg",
+        "img2": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/EVERPHOTO_e49f3b5bef1215d0f411552d90050244.jpg",
+        "img3": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/EVERPHOTO_7fec07c5e311b46a188e778448979bdb.jpg"
     },
     {
         "name": "孙林",
         "detail": "熊秋秋，祝你生日快乐！同时也祝你和德银长长久久。\n--2017/09/25",
         "music": "https://c.y.qq.com/v8/playsong.html?songmid=000LBcVm0d9raf&ADTAG=myqq&from=myqq&channel=10007100",
         "type": "3",
-        "head": "https://media.everphoto.cn/180x180/6469593702355436046.webp?media_token=18a727f557c56cd903420d311dbbe30e",
+        "head": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/-5cac393d6c194194.jpg",
         "qq": "1649803158",
-        "img1": "https://media.everphoto.cn/180x180/6469593696219185677.webp?media_token=555d84293cd983a672f4d0f7c44ce8fc",
-        "img2": "https://media.everphoto.cn/180x180/6469593702355436046.webp?media_token=18a727f557c56cd903420d311dbbe30e",
-        "img3": "https://media.everphoto.cn/180x180/6469593702577734157.webp?media_token=b8f1a75d0c0e6b5827c294004e1fc64b"
+        "img1": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/-5cac393d6c194194.jpg",
+        "img2": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/15d92b6546a23498.jpg",
+        "img3": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/2a4fc261ca74f92c.jpg"
     },
     {
         "name": "赵梓强",
         "detail": "飞花飘絮，霓裳翩翩起舞，几多情愫心飞扬。广袖流云，琴曲指夹凝，清水芙蕖脱尘嚣。\n--2017/09/25",
         "music": "nomusic",
-        "type": "1",
-        "head": "https://media.everphoto.cn/180x180/6469593673658024461.webp?media_token=e43f293245a3a2bec02dc366ba21b71c",
+        "type": "3",
+        "head": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/1649803158CEDEF823805A294BB224AAAE1647EC15.jpg",
         "qq": "1303701792",
-        "img1": "https://media.everphoto.cn/180x180/6469593696114311693.webp?media_token=1f5de768e685f31a953f0778c128f576",
-        "img2": "http://suo.im/3s53JZ",
-        "img3": "http://suo.im/3cAISf"
+        "img1": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/-c10f638287aca41.jpg",
+        "img2": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/1649803158CEDEF823805A294BB224AAAE1647EC15.jpg",
+        "img3": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/c24adda9165a153.jpg"
     },
     {
         "name": "蓝勇",
         "detail": "早点和我兄弟结婚，这杯酒都等了好几年了，熊大，生日快乐。\n--2017/09/25",
         "music": "https://c.y.qq.com/v8/playsong.html?songmid=004XzVtI1YsLfx&ADTAG=myqq&from=myqq&channel=10007100",
         "type": "1",
-        "head": "https://media.everphoto.cn/180x180/6469593647762375181.webp?media_token=ac341b8ce70f219e38a0942351009e90",
+        "head": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/25015522053E533F132E070E813152A2D17F5052A4.jpg",
         "qq": "2501552205",
-        "img1": "https://media.everphoto.cn/180x180/6469593647963701774.webp?media_token=4e57b8e4fa95f2e086b6771359c83d09",
+        "img1": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/-757c0a1e54b92f74.jpg",
         "img2": "",
         "img3": ""
     },
@@ -48,9 +48,9 @@
         "detail": "点右上角查看何二妹给猫猫的信\n--2017/09/25",
         "music": "http://viewer.shouzhangapp.com/H5share/1090579_9YA9USKRZY?app=shouzhang",
         "type": "1",
-        "head": "https://media.everphoto.cn/180x180/6470102688863355406.webp?media_token=856818d38d5dad2df8ca60bd4f5d501b",
+        "head": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/7295852057646240B0702C92DA812C316DEB10F44.jpg",
         "qq": "729585205",
-        "img1": "https://media.everphoto.cn/180x180/6470102690977301006.webp?media_token=590c85aaf165da526564682936d8cd88",
+        "img1": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/513daecd1cb394cc.jpg",
         "img2": "",
         "img3": ""
     },
@@ -59,29 +59,29 @@
         "detail": "愿你：在此后的人生，幸运会如影随形，能够拥有一生一世的追求；让幸福伴随你慢慢成功。你的未来会有很大美丽的惊喜，就像你会渐渐长成一位美丽可爱的他要倾尽此生守护的菇凉，想把所有的祝福都给你，例如：“生日快乐，秋秋！”\n--2017/09/25",
         "music": "nomusic",
         "type": "3",
-        "head": "https://media.everphoto.cn/180x180/6470102680608981517.webp?media_token=1ffed53694cd17a1d28a5515e2b6c72d",
+        "head": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/1548270653ABBCFDBF71A383D43904FEEC40CA7801.jpg",
         "qq": "1548270653",
-        "img1": "https://media.everphoto.cn/180x180/6470102688393593358.webp?media_token=7a120538c55d56b15637810ae59ade73",
-        "img2": "https://media.everphoto.cn/180x180/6409436524579914246.webp?media_token=493b734bc87893b2ca4ed8b023cb4634",
-        "img3": "https://media.everphoto.cn/180x180/6470102680608981517.webp?media_token=1ffed53694cd17a1d28a5515e2b6c72d"
+        "img1": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/-776ba76a894f0e1f.jpg",
+        "img2": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/1548270653ABBCFDBF71A383D43904FEEC40CA7801.jpg",
+        "img3": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/-7a52039be6ad5101.png"
     },
     {
         "name": "夏永峰",
         "detail": "何大嫂，生日快乐！在接下来的岁月里，希望你的事业蒸蒸日上，微商店越做越大，身价超千万。还有和何大哥，岁数到了就赶紧把证儿办了，咱们都等着喝喜酒了，我们非常看好你俩噢！微薄祝福，言辞力弱，但礼轻情意重，望安好！\n--2017/09/25",
         "music": "https://c.y.qq.com/v8/playsong.html?songmid=002BIjxb1Erg6e&ADTAG=myqq&from=myqq&channel=10007100",
         "type": "3",
-        "head": "https://media.everphoto.cn/180x180/6470102672006447629.webp?media_token=278e08ec966d69711b809dfce4afc10e",
+        "head": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/84456603064A19147BFA118EC0641C5EB9FD5D49D.jpg",
         "qq": "844566030",
-        "img1": "https://media.everphoto.cn/180x180/6470102701114917390.webp?media_token=329ca069e472c8fb2b1b8126e94706d5",
-        "img2": "https://media.everphoto.cn/180x180/6470102698124378637.webp?media_token=647edbdef64c6a872214fd3d63287f3a",
-        "img3": "https://media.everphoto.cn/180x180/6470102698254402061.webp?media_token=faf9ddad6501efd84af31369f00a2d28"
+        "img1": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/-1e5d0cd0b544747a.jpg",
+        "img2": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/-6a020f212de58fd8.jpg",
+        "img3": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/615380980dc3ca94.jpg"
     },
     {
         "name": "陈洋-林嫂",
         "detail": "如果风可以解语，我愿意\n为你向风借来，快乐的源泉\n就在今天，为你写下祝福\n秋，生日快乐。\n--2017/09/25",
         "music": "nomusic",
         "type": "2",
-        "head": "https://media.everphoto.cn/180x180/6470102667233329677.webp?media_token=1b9f02960d2a4d01b4e228001ce1487d",
+        "head": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/2367699568ED3A212AD4A4B551F79CD1E416B16DE9.jpg",
         "qq": "2367699568",
         "img1": "",
         "img2": "",
@@ -92,20 +92,20 @@
         "detail": "阿秋: 秋秋啊，还记得你以前早上会说，宇星，起床了，秋秋，你真的很好，我也很喜欢你，上大学联系越来越少了，不过我还是会经常看见你发朋友圈，看你录的那些小视频，可能我对你卖的产品确实兴趣不大，但是我觉得应该支持你，一开始我还怕你被骗，不过现在看来还是不错的，加油啊，秋秋！以前你总说羡慕我和小白，现在该不羡慕了吧，你们家大神也很细心哟！希望秋秋一直幸福，还有就是要是有什么事就跟我说，没事找我聊聊天，我会是很好的倾诉者，还有对待爱情哈要有主动权，嘻嘻！当然，今天是我们秋秋22岁生日，一定要开开心心的！以后啊也一定要幸福，我希望你能一直简单并且有点小目标然后幸福的生活！么么哒！\n--2017/09/26",
         "music": "nomusic",
         "type": "3",
-        "head": "https://media.everphoto.cn/180x180/6470102660195287566.webp?media_token=8cd1c4b6a0580017744cbd7fb8b4100c",
+        "head": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/18224505798BD9C401351882236432E0DF52E3CB7D.jpg",
         "qq": "1822450579",
-        "img1": "https://media.everphoto.cn/180x180/6470102663924023821.webp?media_token=3b45246ba7d31faeea8f903c57f64afd",
-        "img2": "https://media.everphoto.cn/180x180/6470102663370375693.webp?media_token=4cc4ec7eb450509d9c5a9b708478341a",
-        "img3": "https://media.everphoto.cn/180x180/6470102662925779469.webp?media_token=62105a95b0804d128c1fc097197533d9"
+        "img1": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/-74cef1e5e036e7b.jpg",
+        "img2": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/-6060f04db60123b3.jpg",
+        "img3": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/1b97efab79d0bcc6.jpg"
     },
     {
         "name": "郭远梅",
         "detail": "每年都有一个特别的日子，每年都有很多特别的人；人说岁月是把杀猪刀 ，我道岁月是面魔法镜，让你的容颜越发的美艳动人。2017年 小郭祝秋秋生日快乐！祝：两口子感情越来越好，微商事业越做越好！\n--2017/09/26",
         "music": "nomusic",
         "type": "1",
-        "head": "https://media.everphoto.cn/180x180/6470102659645833741.webp?media_token=f47c22b2a116b9c3823de4e7542bab3a",
+        "head": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/1572443496465D6CFF4CAA5CB79BC78C34E94637B3.jpg",
         "qq": "2907232574",
-        "img1": "https://media.everphoto.cn/180x180/6470102659566158350.webp?media_token=afa1dc61a7513b4322363158163a5807",
+        "img1": "https://coding.net/u/QiuSunshine/p/HeartTree/git/raw/master/579be04ed3015ffb.jpg",
         "img2": "",
         "img3": ""
     }
